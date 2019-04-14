ec36e0527de3db56a5b1c1bb7997c87ecad2ef79
Mitchell "Mitch" Guthrie <mitchell.guthrie13@gmail.com>
2019-04-13 22:29:13 -0500

    Tile and Level system
    Fixed bugs and completed title/level system

--- game/Game.java
+++ game/Game.java
@@ -14,8 +14,10 @@ import gfx.Colors;
 import gfx.Font;
 import gfx.Screen;
 import gfx.SpriteSheet;
+import level.Level;
 
 public class Game extends Canvas implements Runnable {
+
 	private static final long serialVersionUID = 1L;
 
 	public static final int WIDTH = 160;
@@ -30,43 +32,46 @@ public class Game extends Canvas implements Runnable {
 
 	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
-	private int[] colors = new int[6 * 6 * 6];
+	private int[] colours = new int[6 * 6 * 6];
 
 	private Screen screen;
 	public InputHandler input;
+	public Level level;
 
 	public Game() {
 		setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
 		setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
 		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+
 		frame = new JFrame(NAME);
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
 		frame.add(this, BorderLayout.CENTER);
 		frame.pack();
+
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
 
 	public void init() {
-		// populating color index
 		int index = 0;
 		for (int r = 0; r < 6; r++) {
 			for (int g = 0; g < 6; g++) {
 				for (int b = 0; b < 6; b++) {
-					// gives shades 0-5
 					int rr = (r * 255 / 5);
 					int gg = (g * 255 / 5);
 					int bb = (b * 255 / 5);
 
-					colors[index++] = rr << 16 | gg << 8 | bb;
+					colours[index++] = rr << 16 | gg << 8 | bb;
 				}
 			}
 		}
 
 		screen = new Screen(WIDTH, HEIGHT, new SpriteSheet("/res/sprite_sheet.png"));
 		input = new InputHandler(this);
+		level = new Level(64, 64);
 	}
 
 	public synchronized void start() {
@@ -80,17 +85,14 @@ public class Game extends Canvas implements Runnable {
 
 	public void run() {
 		long lastTime = System.nanoTime();
-		double nsPerTick = 1000000000D / 60D;
-
-		int frames = 0;
-		int ticks = 0;
-
 		long lastTimer = System.currentTimeMillis();
+		double nsPerTick = 1000000000D / 60D;
 		double delta = 0;
+		int ticks = 0;
+		int frames = 0;
 
 		init();
 
-		// Game loop
 		while (running) {
 			long now = System.nanoTime();
 			delta += (now - lastTime) / nsPerTick;
@@ -103,13 +105,11 @@ public class Game extends Canvas implements Runnable {
 				delta -= 1;
 				shouldRender = true;
 			}
-
 			try {
 				Thread.sleep(2);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-
 			if (shouldRender) {
 				frames++;
 				render();
@@ -117,28 +117,33 @@ public class Game extends Canvas implements Runnable {
 
 			if (System.currentTimeMillis() - lastTimer >= 1000) {
 				lastTimer += 1000;
-				System.out.println(ticks + " ticks, " + frames + " frames");
+				System.out.println(ticks + " ticks , " + frames + " frames per second");
 				frames = 0;
 				ticks = 0;
 			}
 		}
 	}
 
+	private int x = 0;
+	private int y = 0;
+
 	public void tick() {
 		tickCount++;
 
 		if (input.up.isPressed()) {
-			screen.yOffset--;
+			y -= 1;
 		}
 		if (input.down.isPressed()) {
-			screen.yOffset++;
+			y += 1;
 		}
 		if (input.left.isPressed()) {
-			screen.xOffset--;
+			x -= 1;
 		}
 		if (input.right.isPressed()) {
-			screen.xOffset++;
+			x += 1;
 		}
+
+		level.tick();
 	}
 
 	public void render() {
@@ -148,26 +153,30 @@ public class Game extends Canvas implements Runnable {
 			return;
 		}
 
-		for (int y = 0; y < 32; y++) {
-			for (int x = 0; x < 32; x++) {
-				boolean flipX = x % 2 == 1;
-				boolean flipY = y % 2 == 1;
-				// shifting by 3 multiplys them by 8
-				screen.render(x << 3, y << 3, 0, Colors.get(000, 505, 005, 555), flipX, flipY);
-			}
-		}
+		int xOffset = x - (screen.width / 2);
+		int yOffset = y - (screen.height / 2);
 
-		Font.render("Hello World", screen, 0, 0, Colors.get(-1, -1, -1, 000));
+		level.renderTiles(screen, xOffset, yOffset);
+
+		for (int x = 0; x < level.width; x++) {
+			int colour = Colors.get(-1, -1, -1, 000);
+			if (x % 10 == 0 && x != 0) {
+				colour = Colors.get(-1, -1, -1, 500);
+			}
+			Font.render((x % 10) + "", screen, 0 + (x * 8), 0, colour);
+		}
 
 		for (int y = 0; y < screen.height; y++) {
 			for (int x = 0; x < screen.width; x++) {
-				int colorCode = screen.pixels[x + y * screen.width];
-				if (colorCode < 255)
-					pixels[x + y * WIDTH] = colors[colorCode];
+				int ColourCode = screen.pixels[x + y * screen.width];
+				if (ColourCode < 255) {
+					pixels[x + y * WIDTH] = colours[ColourCode];
+
+				}
 			}
 		}
+
 		Graphics g = bs.getDrawGraphics();
-		g.fillRect(0, 0, getWidth(), getHeight());
 		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
 		bs.show();
@@ -176,4 +185,5 @@ public class Game extends Canvas implements Runnable {
 	public static void main(String[] args) {
 		new Game().start();
 	}
+
 }
--- gfx/Screen.java
+++ gfx/Screen.java
@@ -21,34 +21,46 @@ public class Screen {
 		this.sheet = sheet;
 
 		pixels = new int[width * height];
+
 	}
 
-	public void render(int xPos, int yPos, int tile, int color) {
-		render(xPos, yPos, tile, color, false, false);
+	public void setOffset(int xOffset, int yOffset) {
+		this.xOffset = xOffset;
+		this.yOffset = yOffset;
 	}
 
-	public void render(int xPos, int yPos, int tile, int color, boolean mirrorX, boolean mirrorY) {
+	public void render(int xPos, int yPos, int tile, int colour) {
+		render(xPos, yPos, tile, colour, false, false);
+	}
+
+	public void render(int xPos, int yPos, int tile, int colour, boolean mirrorX, boolean mirrorY) {
 		xPos -= xOffset;
 		yPos -= yOffset;
 
 		int xTile = tile % 32;
 		int yTile = tile / 32;
 		int tileOffset = (xTile << 3) + (yTile << 3) * sheet.width;
+
 		for (int y = 0; y < 8; y++) {
-			int ySheet = y;
-			if (mirrorY)
-				ySheet = 7 - y;
-			if (y + yPos < 0 || y + yPos >= height)
+			if (y + yPos < 0 || y + yPos >= height) {
 				continue;
+			}
+			int ySheet = y;
+			if (mirrorY) {
+				ySheet = 7 - y;
+			}
 			for (int x = 0; x < 8; x++) {
-				int xSheet = x;
-				if (mirrorX)
-					xSheet = 7 - y;
-				if (x + xPos < 0 || x + xPos >= width)
+				if (x + xPos < 0 || x + xPos >= width) {
 					continue;
-				int col = (color >> (sheet.pixels[xSheet + ySheet * sheet.width + tileOffset] * 8)) & 255;
-				if (col < 255)
+				}
+				int xSheet = x;
+				if (mirrorX) {
+					xSheet = 7 - x;
+				}
+				int col = (colour >> (sheet.pixels[xSheet + ySheet * sheet.width + tileOffset] * 8)) & 255;
+				if (col < 255) {
 					pixels[(x + xPos) + (y + yPos) * width] = col;
+				}
 			}
 		}
 	}
